a65948bd706fb2f501290b71c3b9bfee0276c5fa
Clement <clecocel@gmail.com>
2018-12-03 17:48:13 -0800

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# tspp
-The Simple Python Profiler
+# The Simple Python Profiler - `tspp`
+
+If you are looking for a very simple profiler that lets you measure the time each part of your programs took, `tspp` is for you!
+
+`tspp` typically replaces code that would look like:
+
+```
+start = time.time()
+do_something()
+print "do_something took", time.time() - start
+```
+
+## Example usage
+```
+import time
+from tspp import Profiler
+
+pr = Profiler()
+
+def do_something():
+    time.sleep(0.1)
+
+for i in xrange(10):
+    pr.register("start")
+    do_something()
+    pr.register("end of do_something")
+print pr.profile()
+```
+
+## Install
+`git clone` this repo, `cd` to the repo, then run `pip install ./`.
---
+++ tspp/profiler.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import time
+
+
+class Profiler(object):
+    def __init__(self, profiler_name=""):
+        self.name = profiler_name
+        self.events = []
+        self.event_names = {}
+        self.last_event = None
+
+    def register(self, name):
+        now = time.time()
+        if name not in self.event_names:
+            self.event_names[name] = len(self.events)
+            if self.last_event is not None:
+                self.events.append([name, now - self.last_event])
+            else:
+                self.events.append([name, 0])
+        else:
+            self.events[self.event_names[name]][1] += now - \
+                self.last_event
+        self.last_event = now
+
+    def clear(self):
+        self.__init__()
+
+    def profile(self):
+        if len(self.events) > 0:
+            total_time = 0
+            for i in range(0, len(self.events)):
+                total_time += self.events[i][1]
+            profile_str = "Profiling {}".format(self.name)
+            profile_str += "\n\tTotal: {}".format(total_time)
+
+            for i in range(0, len(self.events)):
+                profile_str += ("\n\t" +
+                                self.events[i][0] + " " +
+                                str(self.events[i][1]) + " " +
+                                "{0:.2f}".format(
+                                    self.events[i][1] / total_time * 100) +
+                                "%")
+            return profile_str
+        else:
+            return ("Nothing to profile!")
